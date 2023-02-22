32d6d9e97d6908d59723545d434bdeeacd666d2d
bhagyashree.chhajed <bhagyashree.chhajed@jetsynthesys.com>
2023-02-22 18:24:57 +0530

    Tet API code

---
+++ fileTutorial.js
@@ -0,0 +1,30 @@
+import fs from 'fs';
+//CRUD for sync 
+// fs.mkdirSync('thappa');
+// fs.writeFileSync('read.txt',"Welcome vbvbvb bbghgh")
+// fs.writeFileSync('read.txt',"BHC")
+// fs.appendFileSync('read.txt',"BHC append");
+// console.log(fs.readFileSync('read.txt').toString());
+// fs.renameSync('read.txt','read2.txt');
+// console.log(fs.readFileSync('read2.txt','utf-8'));
+// fs.unlinkSync('read2.txt');
+// fs.rmdirSync('thappa');
+
+//CRUD for Asyncsync 
+
+// fs.writeFile('read.txt',"bhc...",(err)=>{
+//     console.log(err); //null
+// });
+
+// fs.appendFile('read.txt',"bhc...",(err)=>{
+// });
+
+// fs.readFile('read.txt',"utf-8",(err,data)=>{
+//     console.log(data)
+// });
+
+// fs.mkdir('thappa',(err,data)=>{
+//     console.log(data)
+// });
+// Note:: Async is only without keyword synv
+
